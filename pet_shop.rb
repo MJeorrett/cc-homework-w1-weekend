156aa96a7838736fd58bf7546d969b7e978c398a
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-09-30 14:38:52 +0100

    Added answers to tests test_find_pet_by_name to test_sell_pet_to_customer__insufficient_funds

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,5 @@
+require('pry-byebug')
+
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
@@ -35,4 +37,41 @@ def find_pet_by_name(pet_shop, name)
     return pet if pet[:name] == name
   end
   return nil
+end
+
+def remove_pet_by_name(pet_shop, name)
+  pets = pet_shop[:pets]
+  for pet in pets
+    if pet[:name] == name
+      pets.delete(pet)
+      return nil
+    end
+  end
+end
+
+def add_pet_to_stock(pet_shop, pet)
+  pet_shop[:pets] += [pet]
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count
+end
+
+def add_pet_to_customer(customer, pet)
+  customer[:pets] += [pet]
+end
+
+def customer_can_afford_pet(customer, pet)
+  return customer[:cash] >= pet[:price]
+end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  return nil if !pet_shop[:pets].include?(pet) || !customer_can_afford_pet(customer, pet)
+  pet_price = pet[:price]
+  customer[:cash] -= pet_price
+  pet_shop[:admin][:total_cash] += pet_price
+  pet_shop[:pets] -= [pet]
+  pet_shop[:admin][:pets_sold] += 1
+  customer[:pets] += [pet]
+  return true
 end
